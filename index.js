435f16260cb365060deb381e63d2f72b5d90634e
Fabricio Sousa <fabricio_370@hotmail.com>
2022-11-02 23:20:45 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,81 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a -b);
+    this.items.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if(pos >= 0 && pos <= this.length -1) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds') 
+    }
+  }
 
-  min() {}
+  max() {
 
-  sum() {}
+    let maximum = this.items[0];
 
-  avg() {}
+  if(this.items.length === 0) {
+    throw new Error('EmptySortedList');
+  } else {
+    for(let i = 0; i <= this.items.length; i++) {
+      if(this.items[i] > maximum) {
+        maximum = this.items[i];
+      }
+    }
+    return maximum
+  }
+ }
+
+  min() {
+
+    let minimum = this.items[0];
+
+  if(this.items.length === 0) {
+    throw new Error('EmptySortedList');
+  } else {
+    for(let i = 0; i <= this.items.length; i++) {
+      if(this.items[i] < minimum) {
+        minimum = this.items[i];
+      }
+    }
+    return minimum
+  }
+  }
+
+  sum(item) {
+  let result = 0;
+  if (item.length === 0) {
+    return 0
+  }
+  for (let i = 0; i < item.length; i++) {
+    result += item[i]
+  }
+  return result;
+  }
+  
+  avg() {
+    let total = 0;
+    let count = 0;
+
+  if (this.items.length === 0) {
+    throw new Error('EmptySortedList');
+  }
+  this.items.forEach(function(item, index) {
+      total += item;
+      count++;
+  });
+
+  return total / count;
+  }
 }
 
 module.exports = SortedList;
